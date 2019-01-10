02fc8cf4fdf503455bcea953084414c4447955b7
Torben Werner <torben@sendwithus.com>
2019-01-10 10:37:39 -0800

    Initial concept

---
+++ packages/now-go-mod/index.js
@@ -0,0 +1,202 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+const path = require('path');
+const { mkdirp, readFile, writeFile } = require('fs-extra');
+const fs = require('fs');
+
+const execa = require('execa');
+const { createLambda } = require('@now/build-utils/lambda.js');
+const getWritableDirectory = require('@now/build-utils/fs/get-writable-directory.js');
+const download = require('@now/build-utils/fs/download.js');
+const downloadGit = require('lambda-git');
+const glob = require('@now/build-utils/fs/glob.js');
+const downloadGoBin = require('./download-go-bin');
+
+// creates a `$GOPATH` directory tree, as per
+// `go help gopath`'s instructions.
+// without this, Go won't recognize the `$GOPATH`
+async function createGoPathTree(goPath) {
+  await mkdirp(path.join(goPath, 'bin'));
+  await mkdirp(path.join(goPath, 'pkg', 'linux_amd64'));
+}
+
+exports.config = {
+  maxLambdaSize: '10mb',
+  lambdaBaseDir: 'cmd',
+  lambdaFileName: 'lambda.go'
+};
+
+async function buildGoLambda({
+  goBin,
+  goEnv,
+  downloadedFiles,
+  outDir,
+  entrypoint
+}) {
+  console.log(`parsing AST for "${entrypoint}"`);
+  let handlerFunctionName = '';
+  try {
+    handlerFunctionName = await execa.stdout(
+      path.join(__dirname, 'bin', 'get-exported-function-name'),
+      [downloadedFiles[entrypoint].fsPath]
+    );
+  } catch (err) {
+    console.log(`failed to parse AST for "${entrypoint}"`);
+    throw err;
+  }
+
+  if (handlerFunctionName === '') {
+    const e = new Error(
+      `Could not find an exported function on "${entrypoint}"`
+    );
+    console.log(e.message);
+    throw e;
+  }
+
+  console.log(
+    `Found exported function "${handlerFunctionName}" on "${entrypoint}"`
+  );
+
+  const origianlMainGoContents = await readFile(
+    path.join(__dirname, 'main.go'),
+    'utf8'
+  );
+  const mainGoContents = origianlMainGoContents.replace(
+    '__NOW_HANDLER_FUNC_NAME',
+    handlerFunctionName
+  );
+  // in order to allow the user to have `main.go`, we need our `main.go` to be called something else
+  const mainGoFileName = 'main__now__go__.go';
+
+  const entrypointDirname = path.dirname(downloadedFiles[entrypoint].fsPath);
+
+  // we need the Now bridge `main.go` in the same dir as the entrypoint,
+  await writeFile(path.join(entrypointDirname, mainGoFileName), mainGoContents);
+
+  console.log('running go build...');
+  try {
+    await execa(
+      goBin,
+      [
+        'build',
+        '-mod=vendor',
+        '-o',
+        path.join(outDir, path.dirname(entrypoint), 'handler'),
+        path.join(entrypointDirname, mainGoFileName),
+        downloadedFiles[entrypoint].fsPath
+      ],
+      { env: goEnv, cwd: entrypointDirname, stdio: 'inherit' }
+    );
+  } catch (err) {
+    console.log('failed to `go build`');
+    throw err;
+  }
+
+  return createLambda({
+    files: await glob('**', path.join(outDir, path.dirname(entrypoint))),
+    handler: 'handler',
+    runtime: 'go1.x',
+    environment: {}
+  });
+}
+
+exports.build = async ({ files, entrypoint, config }) => {
+  /*
+   * Setup Go build environment
+   */
+
+  console.log('downloading files...');
+
+  const gitPath = await getWritableDirectory();
+  const goPath = await getWritableDirectory();
+  const srcPath = path.join(goPath, 'src', 'lambda');
+  const outDir = await getWritableDirectory();
+
+  await createGoPathTree(goPath);
+
+  const downloadedFiles = await download(files, srcPath);
+  const entrypointDirname = path.dirname(downloadedFiles[entrypoint].fsPath);
+
+  console.log('downloading go binary...');
+  const goBin = await downloadGoBin();
+
+  console.log('downloading git binary...');
+  // downloads a git binary that works on Amazon Linux and sets
+  // `process.env.GIT_EXEC_PATH` so `go(1)` can see it
+  await downloadGit({ targetDirectory: gitPath });
+
+  const goEnv = {
+    ...process.env,
+    GOOS: 'linux',
+    GOARCH: 'amd64',
+    GOPATH: goPath,
+    CGO_ENABLED: '0',
+    GO111MODULE: 'on'
+  };
+
+  /*
+   * Install dependencies
+   */
+  console.log('entrypoint listing:');
+  // eslint-disable-next-line arrow-parens
+  fs.readdirSync(entrypointDirname).forEach(file => {
+    console.log(file);
+  });
+  console.log(`checking dependencies of ${entrypointDirname}`);
+  const vendorDir = path.join(entrypointDirname, 'vendor');
+  if (!fs.existsSync(vendorDir)) {
+    console.log(`vendor directory '${vendorDir}'`);
+    try {
+      await execa(goBin, ['mod', 'vendor'], {
+        env: goEnv,
+        cwd: entrypointDirname,
+        stdio: 'inherit'
+      });
+    } catch (err) {
+      console.log('failed to vendor dependencies');
+      throw err;
+    }
+  } else {
+    console.log('vendoring found');
+  }
+
+  /*
+   * Find Go lambda source files
+   */
+  const lambdasDir = path.join(entrypointDirname, config.lambdaBaseDir);
+  console.log(
+    `finding lambdas named ${config.lambdaFileName} in ${lambdasDir}`
+  );
+  if (!fs.existsSync(lambdasDir)) {
+    throw new Error(`lambda directory ${lambdasDir} not found`);
+  }
+  const goLambdaFiles = Object.keys(
+    await glob(`**/${config.lambdaFileName}`, lambdasDir)
+  );
+
+  /*
+   * Build all Go lambdas
+   */
+  console.log(`building ${goLambdaFiles.length} lambdas`);
+  const lambdas = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (const f of goLambdaFiles) {
+    const lambdaEntryPoint = path.join(
+      path.dirname(entrypoint),
+      config.lambdaBaseDir,
+      f
+    );
+    // TODO: Replace this because it uses the `go.mod` instead of the `lambdas_dir/f`
+    path.join();
+    // eslint-disable-next-line no-await-in-loop
+    lambdas[lambdaEntryPoint] = await buildGoLambda({
+      goBin,
+      goEnv,
+      downloadedFiles,
+      outDir,
+      entrypoint: lambdaEntryPoint
+    });
+  }
+
+  return lambdas;
+};
